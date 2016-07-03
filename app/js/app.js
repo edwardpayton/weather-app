a5b4a6eb546453970cab29c5004e5e8aeca714ca
ed <edwardjpayton@me.com>
2016-07-03 14:11:34 +0100

    Disabled debug info for production

--- app/js/app.js
+++ app/js/app.js
@@ -3,8 +3,11 @@
 angular.module('weatherApp', [
   'ngRoute',
   'WeatherModule'
-]).
-config(['$routeProvider', function($routeProvider) {
+])
+.config(['$compileProvider', function ($compileProvider) {
+  $compileProvider.debugInfoEnabled(false);
+}])
+.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/about-me', {templateUrl: 'templates/about.html'});
   $routeProvider.when('/', {templateUrl: 'templates/weather.html'});
   $routeProvider.otherwise({redirectTo: '/'});
